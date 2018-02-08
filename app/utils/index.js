6afc49201431a35086b62a11ae99c0607e49d8df
Alena Hrytsuk <Alena_Hrytsuk@epam.com>
2018-02-08 15:04:09 +0300

    refactoring

--- app/utils/index.js
+++ app/utils/index.js
@@ -1,13 +1,10 @@
 module.exports = function() {
   return function(req, res, next) {
       // isAuthenticated is set by `deserializeUser()`
-      if (!req.isAuthenticated || !req.isAuthenticated()) {
-          res.status(401).send({
-              success: false,
-              message: 'You need to be authenticated to access this page!'
-          });
-      } else{
-          next()
-      }
+    if (!req.isAuthenticated || !req.isAuthenticated()) {
+      next({message: 'You need to be authenticated to access this page!'})
+    } else {
+        next()
+    }
   }
 }
